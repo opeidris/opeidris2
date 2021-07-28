d7405ad4d9ea8937febde96e580c21ddd4ae101e
opeidris <opeyemi@opeidris.com>
2021-07-28 11:59:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen bg-purple-700 flex items-center justify-center">
+    <h1 class=" text-6xl uppercase text-white font-black">First Commit</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
